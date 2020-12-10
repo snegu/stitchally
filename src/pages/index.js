cd1b4554bfaca4f48eeefd315b44bd61a38d0424
Sarah <snegugogor@gmail.com>
2020-12-09 16:16:31 -0800

    trying again

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Count stitches with ease</h1>
-    <Link to="/page-2/">Create a new project now</Link>
+    <Link to="/page-2/">Create a new project now for me</Link>
   </div>
 )
 
